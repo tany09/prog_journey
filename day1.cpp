f128adc418b1d443222c62466090798a05e7edd6
tany09 <34600671+tany09@users.noreply.github.com>
2017-12-16 23:10:35 +0530

    update
    1. use of macros
    2. use of typedef

--- day1.cpp
+++ day1.cpp
@@ -1,4 +1,6 @@
 #include<bits/stdc++.h>
+#define REP(i,a,b) for(int i=a;i<=b;i++)
+typedef long long ll;
 using namespace std;
 int main()
 {
@@ -6,9 +8,13 @@ int main()
     cin.tie(0);
    // freopen("output.txt","r",stdin);
     string xq;
+    ll a=10;
 
-    double x=0.3*3+0.1;
-    printf("%.20f\n",x);
+   /* double x=0.3*3+0.1;
+    printf("%.20f\n",x);  */
+    cout <<a<<endl;
+    REP(i,1,10)
+    cout<<a;
 //    cin>>i;
 //    cout<<(-1*i)%3;
 /*while(i<50){
