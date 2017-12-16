70720fb604b4cff1d454ae2ba543600d1e139698
tany09 <34600671+tany09@users.noreply.github.com>
2017-12-16 18:48:55 +0530

    chapter 1
    1.remove sync of cin with scanf.
    2.file handling- i/o stream.
    3. double data type precision
    4.input with spaces and enter
    5.mod operator with negative numbers

---
+++ day1.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+   // freopen("output.txt","r",stdin);
+    string xq;
+
+    double x=0.3*3+0.1;
+    printf("%.20f\n",x);
+//    cin>>i;
+//    cout<<(-1*i)%3;
+/*while(i<50){
+    cout<<(-1*i)%5<<" "<<i%-5<<endl ;
+    i++;
+}*/
+     /*while(cin >> x)
+    {
+        cout << x<< endl;
+
+    }*/
+
+return 0;
+}
